897eec3f3445b19377e493fcd4a13df1dc55c6ae
Oriol7272 <tu-email@ejemplo.com>
2025-08-27 20:13:32 +0200

    chore(env): generar env-inline.js desde Vercel (claves exactas) e inyectar referencia

--- js/env-inline.js
+++ js/env-inline.js
@@ -1,2 +1,23 @@
-/* AUTO-GENERADO. NO EDITAR. */
+/* AUTO-GENERADO DESDE VERCEL ENV (PROD). NO EDITAR. */
 ;(function(){ window.IBG_ENV = window.IBG_ENV || {};
+  window.IBG_ENV["PAYPAL_CLIENT_ID"] = "\"AfQEdiielw5fm3wF08p9pcxwqR3gPz82YRNUTKY4A8WNG9AktiGsDNyr2i7BsjVzSwwpeCwR7Tt7DPq5\"";
+  window.IBG_ENV["PAYPAL_SECRET"] = "\"EGeBAStDv-G2iALPalvnyERyNFnInPCI03pgP_rscAKaszgJTKzJwz4kbFQx_XeKfPVS4HLGuhM533Dv\"";
+  window.IBG_ENV["PAYPAL_WEBHOOK_ID"] = "\"7YP450914N8142540\"";
+  window.IBG_ENV["PAYPAL_PLAN_MONTHLY_1499"] = "\"P-3WE8037612641383DNCUKNJI\"";
+  window.IBG_ENV["PAYPAL_PLAN_ANNUAL_4999"] = "\"P-43K261214Y571983RNCUKN7I\"";
+  window.IBG_ENV["PAYPAL_ONESHOT_PRICE_EUR_LIFETIME"] = "\"100.00\"";
+  window.IBG_ENV["PAYPAL_ONESHOT_PRICE_EUR_VIDEO"] = "\"0.30\"";
+  window.IBG_ENV["PAYPAL_ONESHOT_PRICE_EUR_IMAGE"] = "\"0.10\"";
+  window.IBG_ENV["PAYPAL_ONESHOT_PACK10_IMAGES_EUR"] = "\"0.80\"";
+  window.IBG_ENV["PAYPAL_ONESHOT_PACKS_VIDEOS_EUR"] = "";
+  window.IBG_ENV["IBG_ASSETS_BASE_URL"] = "\"https://ibizagirl-assets.s3.eu-north-1.amazonaws.com\"";
+  window.IBG_ENV["EXOCLICK_ZONE"] = "\"5696328\"";
+  window.IBG_ENV["JUICYADS_ZONE"] = "\"1099637\"";
+  window.IBG_ENV["EROADVERTISING_ZONE"] = "\"8177575\"";
+  window.IBG_ENV["JUICYADS_SNIPPET_B64"] = "\"2093981\"";
+  window.IBG_ENV["EROADVERTISING_SNIPPET_B64"] = "\"2093981\"";
+  window.IBG_ENV["POPADS_ENABLE"] = "\"true\"";
+  window.IBG_ENV["POPADS_SITE_ID"] = "\"e494ffb82839a29122608e933394c091\"";
+  window.IBG_ENV["CRISP_WEBSITE_ID"] = "\"59e184b1-e679-4c93-b3ea-d60b63c1c04c\"";
+  console.log("[env-inline] OK ->", Object.keys(window.IBG_ENV));
+})();
